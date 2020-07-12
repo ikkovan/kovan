a76ad20b7b082285f0b783a1f19b799dc2cc741a
cserko <m.serkantan@gmail.com>
2020-07-12 19:39:17 +0300

    sistemin omuragesı eklendi!

---
+++ kovan/src/main/java/com/ik/kovan/KovanApplication.java
@@ -0,0 +1,13 @@
+package com.ik.kovan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KovanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KovanApplication.class, args);
+	}
+
+}
---
+++ kovan/src/test/java/com/ik/kovan/KovanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ik.kovan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KovanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
