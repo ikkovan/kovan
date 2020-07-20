8ac71f0f9a7c2440075a19454d0de10c98b6de9f
serkan <m.serkantan@gmail.com>
2020-07-20 06:11:34 -0400

    Registration functionality added!

--- kovan/src/main/java/com/ik/kovan/controller/EmployeeController.java
+++ kovan/src/main/java/com/ik/kovan/controller/EmployeeController.java
@@ -9,11 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-
+import javax.validation.Valid;
 /***
  * @author serkantan
  */
@@ -46,9 +46,27 @@ public class EmployeeController {
 
 
     @GetMapping("/register")
-    public String employeeForm(){
-        //model.addAttribute("greeting", new Employee());
-        return "registerUser.html";
+    public String getEmployee(Model model){
+        model.addAttribute("employee", new Employee());
+        return "registerUser";
+    }
+
+    @PostMapping("/register")
+    public String addEmployee(@Valid @ModelAttribute("employee")Employee employee, BindingResult result, Model model){
+        System.out.println("This is Employee Registration Controller.");
+        if (result.hasErrors())
+        {
+            System.out.println("Post method has some errors!");
+            System.out.println(result);
+            return "registerUser";
+        }
+
+        else
+        {
+            employeeService.save(employee);
+            return "registerUser";
+        }
+
     }
 
 
