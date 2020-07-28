85d6c77bdad22efb4674f07e15c253d8db17cc22
serkan <m.serkantan@gmail.com>
2020-07-28 13:09:50 -0400

    Statement Model part is added

---
+++ kovan/src/main/java/com/ik/kovan/service/impl/StatementImpl.java
@@ -0,0 +1,36 @@
+package com.ik.kovan.service.impl;
+
+import com.ik.kovan.model.Statement;
+import com.ik.kovan.repository.StatementRepository;
+import com.ik.kovan.service.service.StatementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StatementImpl implements StatementService {
+
+    @Autowired
+    StatementRepository statementRepository;
+
+    @Override
+    public Statement findById(int id) {
+        return statementRepository.findById(id);
+    }
+
+    @Override
+    public List<Statement> listStatement() {
+        return listStatement();
+    }
+
+    @Override
+    public Statement save(Statement statement) {
+        return save(statement);
+    }
+
+    @Override
+    public void delete(Statement statement) {
+        delete(statement);
+    }
+}
