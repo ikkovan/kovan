9265c0bf919399b5bb5fd8b7107cdf47ae7e3cda
Sibel Keleş <sibelkeles1997@gmail.com>
2020-08-12 19:43:15 +0300

    server url changed

--- kovan/kovan-front/src/app/service/payroll.service.ts
+++ kovan/kovan-front/src/app/service/payroll.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PayrollService {
-  private baseUrl = 'http://localhost:8080/payroll';
+  private baseUrl = 'http://35.222.205.42:8080/';
 
   constructor(private http: HttpClient) { }
 
